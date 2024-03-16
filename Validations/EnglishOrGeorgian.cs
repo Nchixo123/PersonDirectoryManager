d94ae339bd8f2ba862b94320badb99dee92fd2af
Nick <nick.chikhladze123@gmail.com>
2024-03-16 05:50:28 +0400

    ncv

---
+++ Validations/EnglishOrGeorgian.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Validations;
+
+public class EnglishOrGeorgian : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is string str)
+        {
+            string englishPattern = @"^[a-zA-Z]*$";
+            string georgianPattern = @"^[ა-ჰ]*$";
+
+            if (Regex.IsMatch(str, englishPattern, RegexOptions.IgnoreCase) || Regex.IsMatch(str, georgianPattern))
+            {
+                return ValidationResult.Success;
+            }
+        }
+
+        return new ValidationResult("The string can only contain English or Georgian characters, not both.");
+    }
+}
