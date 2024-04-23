c05bb2085fd4979b867916f01e1d9cd91fcf94af
Nika Chikhladze <nick.chikhladze123@gmail.com>
2024-04-23 17:46:27 +0400

    changes

--- Db.Repository/PersonRepository.cs
+++ Db.Repository/PersonRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DTOs;
+using Microsoft.EntityFrameworkCore;
 using Services.RepositoryInterfaces;
 
 namespace Db.Repository;
@@ -8,5 +9,14 @@ internal sealed class PersonRepository : RepositoryBase<Person>, IPersonReposito
     internal PersonRepository(PersonDbContext context) : base(context)
     {
         
+    }
+
+    internal async Task<List<Person>> GetPersonRelationships(int personId)
+    {
+        return await (from p in _context.People
+                      join r in _context.PersonRelations on p.Id equals r.FromId into PersonRelations
+                      from pr in PersonRelations.DefaultIfEmpty()
+                      where (pr.FromId == personId || pr.ToId == personId) && p.Id != personId
+                      select p).ToListAsync();
     }
 }
